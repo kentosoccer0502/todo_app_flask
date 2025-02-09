600b0e5c18aacdd5fd04dd9fdada3cd99516de21
kentosoccer0502 <kentosoccer0502@gmail.com>
2025-02-09 18:24:07 +0900

    developing: todo_update frontend
    * need to fix and clean the html and styles.

--- app/routes/todo.py
+++ app/routes/todo.py
@@ -9,27 +9,6 @@ from app.utils.auth import basic_authenticate
 todo_bp = Blueprint('todo', __name__)
 
 
-@todo_bp.route('/todos/add', methods=['POST'])
-@login_required
-def create_todo():
-    data = request.form
-    try:
-        title = data.get('title')
-        status = data.get('status', '未着手')
-        priority = data.get('priority', 1)
-        todo_new = Todo(
-            title=title,
-            status=status,
-            priority=priority,
-            user_id=current_user.id,
-        )
-        db.session.add(todo_new)
-        db.session.commit()
-        return redirect(url_for('todo.get_todos')), 302
-    except Exception as e:
-        return jsonify({'error': str(e)}), 503
-
-
 @todo_bp.route('/todos', methods=['GET'])
 @login_required
 def get_todos():
@@ -53,36 +32,58 @@ def get_todos():
         return jsonify({'error': str(e)}), 503
 
 
-@todo_bp.route('/todos/update/<int:id>', methods=['PUT'])
-@basic_authenticate
-def update_todo(user, id):
-    data = request.get_json()
+@todo_bp.route('/todos/add', methods=['POST'])
+@login_required
+def create_todo():
+    data = request.form
     try:
-        todo_update = Todo.query.filter(
-            Todo.id == id, Todo.user_id == user.id).first()
-        if not todo_update:
-            return (f'Error: id:{id} does not exists'), 404
-        if 'title' in data:
-            todo_update.todo_title = data.get('title')
-        if 'status' in data:
-            todo_update.todo_status = data.get('status')
-        if 'priority' in data:
-            todo_update.todo_priority = data.get('priority')
+        title = data.get('title')
+        status = data.get('status', '未着手')
+        priority = data.get('priority', 1)
+        todo_new = Todo(
+            title=title,
+            status=status,
+            priority=priority,
+            user_id=current_user.id,
+        )
+        db.session.add(todo_new)
         db.session.commit()
-        return redirect(url_for('todo.main')), 200
+        return redirect(url_for('todo.get_todos')), 302
     except Exception as e:
         return jsonify({'error': str(e)}), 503
 
 
-@todo_bp.route('/todos/delete/<int:todo_id>', methods=['POST'])
+@todo_bp.route('/todos/update/<int:id>', methods=['POST'])
 @login_required
-def delete_todo(todo_id):
+def update_todo(id):
+    data = request.form
+    try:
+        todo_update = Todo.query.filter(
+            Todo.id == id,
+            Todo.user_id == current_user.id).first()
+        if not todo_update:
+            return (f'Error: id:{id} does not exists'), 404
+        if 'title' in data:
+            todo_update.title = data.get('title')
+        if 'status' in data:
+            todo_update.status = data.get('status')
+        if 'priority' in data:
+            todo_update.priority = data.get('priority')
+        db.session.commit()
+        return redirect(url_for('todo.get_todos')), 302
+    except Exception as e:
+        return jsonify({'error': str(e)}), 503
+
+
+@todo_bp.route('/todos/delete/<int:id>', methods=['POST'])
+@login_required
+def delete_todo(id):
     try:
         todo_delete = Todo.query.filter(
-            Todo.id == todo_id,
+            Todo.id == id,
             Todo.user_id == current_user.id).first()
         if not todo_delete:
-            return (f'Error: id{todo_id} does not exists'), 404
+            return (f'Error: id{id} does not exists'), 404
         db.session.delete(todo_delete)
         db.session.commit()
         return redirect(url_for('todo.get_todos')), 302
